2acde22db9535783beeeece662251352a2850cc6
chenjie <18822126754@163.com>
2022-08-28 13:39:22 +0800

    provide和inject以及异步组件分饱

---
+++ vue3/src/02_provide和inject/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <HomeApp></HomeApp>
+    <button @click="addName">dianji</button>
+  </div>
+</template>
+
+<script>
+import HomeApp from './HomeApp.vue';
+import { computed } from 'vue';
+
+export default {
+    components: {
+        HomeApp
+    },
+    provide() {
+        return {
+            name: this.name,
+            age: 18,
+            length: computed(() => this.arr.length)
+        }
+    },
+    data() {
+        return {
+            arr: [1,2,3,4,5,6],
+            name: 'chenjie'
+        }
+    },
+    methods: {
+        addName() {
+            this.arr.push('c');
+            this.name = 'cj'
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
--- vue3/src/main.js
+++ vue3/src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './02_provide和inject/App.vue';
+
+
 
 createApp(App).mount('#app')
