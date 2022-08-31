a2275738bff3b82af804b3c96eb7372c2a71f410
chenjie <18822126754@163.com>
2022-08-31 17:34:40 +0800

    watch监听ref

--- componsition-api/src/06_watch/App.vue
+++ componsition-api/src/06_watch/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div>
-    <h1>{{info.name}} - {{info.age}}</h1>
+    <!-- <h1>{{info.name}} - {{info.age}}</h1> -->
+    <h1>{{info1}} - {{info1}}</h1>
     <button @click="changeName">修改名字</button>
     <button @click="changeAge">修改年龄</button>
   </div>
 </template>
 
 <script>
-import { reactive, watch } from 'vue';
+import { reactive, ref, watch } from 'vue';
 
 export default {
     setup() {
         const info = reactive({name:'cj', age: 18});
+        const info1 = ref('cj')
 
         // watch监听reactive响应式
         // watch(()=>info.name, (newVal, oldVal) => {
@@ -21,30 +23,39 @@ export default {
         // })
 
         // watch监听reactive响应式对象
-        watch(info, (newVal, oldVal) => {
-            console.log(newVal, oldVal);
-            console.log(newVal.name);
-        }, {
-            immediate: false
-        })
+        // watch(info, (newVal, oldVal) => {
+        //     console.log(newVal, oldVal);
+        //     console.log(newVal.name);
+        // }, {
+        //     immediate: false
+        // })
 
-        watch(() => ({...info}), (newVal, oldVal) => {
-            console.log(newVal, oldVal);
+        // watch(() => ({...info}), (newVal, oldVal) => {
+        //     console.log(newVal, oldVal);
+        // }, {
+        //     immediate: false
+        // })
+
+       
+
+        // watch监听ref对象
+        watch(info1, (newVal, oldVal) => {
+            console.log(newVal, oldVal)
         }, {
-            immediate: false
+            deep: true
         })
 
         const changeName = () => {
-            info.name = 'chenjie';
+            info1.value = 'chenjie';
         }
 
         const changeAge = () => {
-            info.age++;
+            info1.value.age++;
         }
-
 
         return {
             info,
+            info1,
             changeAge,
             changeName
         }
